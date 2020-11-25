4a1df5ee9e5a24ca3faf4631a2d8755f04cd3f67
kyleyiyang <kyleyiyang@gmail.com>
2020-11-25 10:48:49 -0500

    Update sumOfLeftLeaves.java

--- sumOfLeftLeaves.java
+++ sumOfLeftLeaves.java
@@ -15,34 +15,49 @@
  */
 class Solution {
     public int sumOfLeftLeaves(TreeNode root) {
-        if (root.left==null && root.right==null) return 0;
+        //if (root.left==null && root.right==null) return 0;
         int sum=0;
         List<Integer> r=new ArrayList<>();
-        searchRight(root,r,0);
+        searchLeft(root,r,0);
+        //searchRight(root,r,0);
         int right=0;
         for (int a:r) {
             right+=a;
         }
-        sum=searchSum(root,sum);
-        System.out.println("sum="+sum+"; r="+r);
-        return sum-right;
+        //sum=searchSum(root,sum);
+        //System.out.println("sum="+sum+"; r="+r);
+        //return sum-right;
+        return right;
     }
     public static int searchSum(TreeNode root, int sum) {
         if (root!=null) {
             sum=searchSum(root.left,sum)+searchSum(root.right,sum);
-            if (root.left==null && root.right==null) sum+=root.val;
+            if (root.left==null && root.right==null) sum=root.val;
         }
         return sum;
     }
     public static int searchRight(TreeNode root, List<Integer> r, int r_sum) {
         if (root!=null) {
             
-            if (root.left!=null) searchRight(root.left,r,r_sum);
-            if (root.right!=null) r_sum+=searchRight(root.right,r,r_sum);
-            if (root.left==null && root.right==null) return r_sum+=root.val;
-            r.add(r_sum);
-            System.out.println("r_sum="+r_sum);
+            searchRight(root.left,r,r_sum);
+            searchRight(root.right,r,r_sum);
+            if (root.right!=null && root.right.left==null && root.right.right==null) {r.add(root.right.val);}
+            
+            System.out.println("root.val="+root.val+"; r_sum="+r_sum);
+            
         }
         return r_sum;
+    }
+    public static int searchLeft(TreeNode root, List<Integer> r, int l_sum) {
+        if (root!=null) {
+            
+            searchLeft(root.left,r,l_sum);
+            searchLeft(root.right,r,l_sum);
+            if (root.left!=null && root.left.left==null && root.left.right==null) {r.add(root.left.val);}
+            
+            //System.out.println("root.val="+root.val+"; r_sum="+r_sum);
+            
+        }
+        return l_sum;
     }
 }
